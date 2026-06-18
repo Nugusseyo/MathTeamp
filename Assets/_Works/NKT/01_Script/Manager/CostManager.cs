c3703aae2e8a77b1fbbd51f1ebceeb9db6184dba
Nam Kitae <197541885+Kitae-Nam@users.noreply.github.com>
2026-06-18 12:26:41 +0900

    feat : costManager edit

--- Assets/_Works/NKT/01_Script/Manager/CostManager.cs
+++ Assets/_Works/NKT/01_Script/Manager/CostManager.cs
@@ -10,7 +10,7 @@ namespace NKT.Manager
         public int Money
         {
             get => _money;
-            set => _money = Mathf.Clamp(value, 0,  int.MaxValue);
+            private set => _money = Mathf.Clamp(value, 0,  int.MaxValue);
         }
         
         public event Action OnMoneyChanged;        
@@ -18,13 +18,11 @@ namespace NKT.Manager
         public void AddMoney(int money)
         {
             _money += money;
-            _money = Mathf.Clamp(_money, 0, int.MaxValue);
             OnMoneyChanged?.Invoke();
         }
         public void SpendMoney(int money)
         {
             _money -= money;
-            _money = Mathf.Clamp(_money, 0, int.MaxValue);
             OnMoneyChanged?.Invoke();
         }
 
