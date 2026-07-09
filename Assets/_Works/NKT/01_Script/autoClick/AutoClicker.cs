d377f0ed85dc4bdc0c50b0f5c2437b410902c52e
Nam Kitae <197541885+Kitae-Nam@users.noreply.github.com>
2026-07-09 12:32:47 +0900

    feat : autoclick

---
+++ Assets/_Works/NKT/01_Script/autoClick/AutoClicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using NKT.Manager;
+using NKT.Upgrade;
+using UnityEngine;
+
+namespace NKT.autoClick
+{
+    public class AutoClicker : MonoBehaviour
+    {
+        [SerializeField] private UpgradeSo upgradeSo;
+        private int _getMoney = 1;
+        public float clickInterval = 2f;
+        public bool isAutoClick = false;
+
+        public void MoneyIncrease(int money)
+        {
+            _getMoney += money;
+        }
+        
+        public void StartAutoClick()
+        {
+            if (isAutoClick) return;
+            StartCoroutine(AutoClickCoroutine());
+        }
+
+        private IEnumerator AutoClickCoroutine()
+        {
+            isAutoClick = true;
+            while (true)
+            {
+                yield return new WaitForSeconds(clickInterval);
+                
+                CostManager.Instance.AddMoney(_getMoney);
+            }
+        }
+    }
+}
