c1d8399f0f240bb0574006d5953d6fd28f0cd1c3
Nugusaeyo <chsj0702@gmail.com>
2026-07-13 10:15:30 +0900

    야ㅑㅑㅑ

---
+++ Assets/_Works/JYG/_Scripts/SceneMove.cs
@@ -0,0 +1,12 @@
+using AH.Code;
+using UnityEngine;
+
+namespace JYG._Scripts
+{
+    public class SceneMove : MonoBehaviour
+    {
+        public SceneManager sceneManager;
+        
+        
+    }
+}
